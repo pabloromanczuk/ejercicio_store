0e0eac64ee9170e013f0b7bcf57f016abc185652
pablo <promanczuk@jubbler.com.ar>
2026-08-17 05:02:45 -0300

    storage folders

---
+++ storage/framework/views/c278874bf6e209942d1352d6630f162d.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo e(config('app.name')); ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7/css/materialdesignicons.min.css" rel="stylesheet">
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.ts']); ?>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
+<?php /**PATH D:\xampp\htdocs\85\promanczuk\ejercicio_store\resources\views/app.blade.php ENDPATH**/ ?>
